6f51d15a64c558854ac23ff71735e885932f93ac
Rob Lourens <roblourens@gmail.com>
2015-08-26 13:47:45 -0700

    Replace node.es6.d.ts with es6 tsd files

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,2 +1,4 @@
+/// <reference path="es6-collections/es6-collections.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
 /// <reference path="node/node.d.ts" />
 /// <reference path="ws/ws.d.ts" />
