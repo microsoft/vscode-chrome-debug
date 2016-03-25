5b91eaed4f18badd052f7e21fb0ff7441e5f3784
James Lissiak <james.lissiak@microsoft.com>
2016-03-25 09:56:20 -0700

    Creating lib folder with build - Now the build task will create a lib folder containing all the .d.ts files which can be used by another extension depending on this one

---
+++ main.ts
@@ -0,0 +1,9 @@
+import {WebKitDebugSession} from './webkit/webKitDebugSession';
+import {WebKitDebugAdapter} from './webkit/webKitDebugAdapter';
+import {WebKitConnection} from './webkit/webKitConnection';
+
+export {
+    WebKitDebugSession,
+    WebKitDebugAdapter,
+    WebKitConnection
+}
