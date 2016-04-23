de30375dbc90a3a498b7ef0d35454bf76a386270
Rob Lourens <roblourens@gmail.com>
2016-04-23 16:11:05 -0700

    Get rid of vscode-debugadapter dependency

--- src/chromeDebug.ts
+++ src/chromeDebug.ts
@@ -3,6 +3,5 @@
  *--------------------------------------------------------*/
 
 import {ChromeDebugSession} from 'vscode-chrome-debug-core';
-import {DebugSession} from 'vscode-debugadapter';
 
-DebugSession.run(<typeof DebugSession><any>ChromeDebugSession);
+ChromeDebugSession.run(ChromeDebugSession);
