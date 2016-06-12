6f5cfbaa44f6a8676b080d0435877fd7ebb497cd
Rob Lourens <roblourens@gmail.com>
2016-06-11 22:26:29 -0700

    Use configurable log file

--- src/chromeDebug.ts
+++ src/chromeDebug.ts
@@ -3,7 +3,11 @@
  *--------------------------------------------------------*/
 
 import {ChromeDebugSession} from 'vscode-chrome-debug-core';
+import * as path from 'path';
 
 // Start a ChromeDebugSession configured to only match 'page' targets, which are Chrome tabs
 ChromeDebugSession.run(ChromeDebugSession.getSession(
-    { targetFilter: target => target && (!target.type || target.type === 'page') }));
+    {
+        targetFilter: target => target && (!target.type || target.type === 'page'),
+        logFileDirectory: path.resolve(__dirname, '../../')
+    }));
