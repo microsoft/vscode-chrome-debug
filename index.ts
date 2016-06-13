c7360ab746ff5b236738e8cb5ace3b91600f47d8
Rob Lourens <roblou@microsoft.com>
2016-06-13 11:05:43 -0700

    Modules get lower-cased names

--- index.ts
+++ index.ts
@@ -5,13 +5,13 @@
 import {ChromeDebugSession} from './src/chrome/chromeDebugSession';
 import {ChromeDebugAdapter} from './src/chrome/chromeDebugAdapter';
 import {ChromeConnection} from './src/chrome/chromeConnection';
-import * as Utils from './src/utils';
-import * as Logger from './src/logger';
+import * as utils from './src/utils';
+import * as logger from './src/logger';
 
 export {
     ChromeDebugSession,
     ChromeDebugAdapter,
     ChromeConnection,
-    Utils,
-    Logger
+    utils,
+    logger
 }
