69adac68b6ca8f4a0f4fe604ebd63fa76d93b895
Manoj Patel <mapatel@microsoft.com>
2016-06-28 11:16:52 -0700

    Fix driveLetter casing in DIRNAME

--- test/sourceMaps/sourceMaps.test.ts
+++ test/sourceMaps/sourceMaps.test.ts
@@ -6,11 +6,14 @@ import * as assert from 'assert';
 import * as mockery from 'mockery';
 import * as testUtils from '../testUtils';
 import * as path from 'path';
+
+import {fixDriveLetterAndSlashes} from '../../src/utils';
 import {SourceMaps} from '../../src/sourceMaps/sourceMaps';
 import {MappedPosition} from '../../src/sourceMaps/sourceMap';
 
 suite('SourceMaps', () => {
-    const DIRNAME = __dirname.toLowerCase();
+    // VSCode expects lowercase windows drive names
+    const DIRNAME = fixDriveLetterAndSlashes(__dirname);
     const GENERATED_PATH = path.resolve(DIRNAME, 'testData/app.js');
     const AUTHORED_PATH = path.resolve(DIRNAME, 'testData/source1.ts');
     const ALL_SOURCES = [AUTHORED_PATH, path.resolve(DIRNAME, 'testData/source2.ts')];
