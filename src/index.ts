3d756c73bf0abf31c4eaa2eb1a536e91e16aee35
Rob Lourens <roblou@microsoft.com>
2016-07-15 14:06:00 -0700

    Export debugAdapterInterfaces In a way that won't output a require in the .js

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@
 
 import {ChromeConnection} from './chrome/chromeConnection';
 import {ChromeDebugAdapter} from './chrome/chromeDebugAdapter';
-import * as Chrome from './chrome/chromeDebugProtocol';
 import {ChromeDebugSession, IChromeDebugSessionOpts} from './chrome/chromeDebugSession';
 import * as chromeTargetDiscoveryStrategy from './chrome/chromeTargetDiscoveryStrategy';
 import * as chromeUtils from './chrome/chromeUtils';
 
-import * as debugAdapterInterfaces from './debugAdapterInterfaces';
 import {AdapterProxy} from './adapterProxy';
 import {LineNumberTransformer} from './transformers/lineNumberTransformer';
 import {SourceMapTransformer} from './transformers/sourceMapTransformer';
+
+export * from './debugAdapterInterfaces';
 
 import * as utils from './utils';
 import * as logger from './logger';
@@ -20,13 +20,11 @@ import * as logger from './logger';
 export {
     ChromeConnection,
     ChromeDebugAdapter,
-    Chrome,
     ChromeDebugSession,
     IChromeDebugSessionOpts,
     chromeTargetDiscoveryStrategy,
     chromeUtils,
 
-    debugAdapterInterfaces,
     AdapterProxy,
     LineNumberTransformer,
     SourceMapTransformer,
