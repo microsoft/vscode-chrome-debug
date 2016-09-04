8b097f1303327cdab22afd21618a385569df19ed
Rob Lourens <roblourens@gmail.com>
2016-09-04 15:50:10 -0700

    Call super.launch

--- src/chromeDebugAdapter.ts
+++ src/chromeDebugAdapter.ts
@@ -12,51 +12,51 @@ export class ChromeDebugAdapter extends CoreDebugAdapter {
     private _chromeProc: ChildProcess;
 
     public launch(args: ILaunchRequestArgs): Promise<void> {
-        this.setupLogging(args);
+        return super.launch(args).then(() => {
+            // Check exists?
+            const chromePath = args.runtimeExecutable || utils.getBrowserPath();
+            if (!chromePath) {
+                return coreUtils.errP(`Can't find Chrome - install it or set the "runtimeExecutable" field in the launch config.`);
+            }
 
-        // Check exists?
-        const chromePath = args.runtimeExecutable || utils.getBrowserPath();
-        if (!chromePath) {
-            return coreUtils.errP(`Can't find Chrome - install it or set the "runtimeExecutable" field in the launch config.`);
-        }
+            // Start with remote debugging enabled
+            const port = args.port || 9222;
+            const chromeArgs: string[] = ['--remote-debugging-port=' + port];
 
-        // Start with remote debugging enabled
-        const port = args.port || 9222;
-        const chromeArgs: string[] = ['--remote-debugging-port=' + port];
+            // Also start with extra stuff disabled
+            chromeArgs.push(...['--no-first-run', '--no-default-browser-check']);
+            if (args.runtimeArgs) {
+                chromeArgs.push(...args.runtimeArgs);
+            }
 
-        // Also start with extra stuff disabled
-        chromeArgs.push(...['--no-first-run', '--no-default-browser-check']);
-        if (args.runtimeArgs) {
-            chromeArgs.push(...args.runtimeArgs);
-        }
+            if (args.userDataDir) {
+                chromeArgs.push('--user-data-dir=' + args.userDataDir);
+            }
 
-        if (args.userDataDir) {
-            chromeArgs.push('--user-data-dir=' + args.userDataDir);
-        }
+            let launchUrl: string;
+            if (args.file) {
+                launchUrl = coreUtils.pathToFileURL(args.file);
+            } else if (args.url) {
+                launchUrl = args.url;
+            }
 
-        let launchUrl: string;
-        if (args.file) {
-            launchUrl = coreUtils.pathToFileURL(args.file);
-        } else if (args.url) {
-            launchUrl = args.url;
-        }
+            if (launchUrl) {
+                chromeArgs.push(launchUrl);
+            }
 
-        if (launchUrl) {
-            chromeArgs.push(launchUrl);
-        }
+            logger.log(`spawn('${chromePath}', ${JSON.stringify(chromeArgs) })`);
+            this._chromeProc = spawn(chromePath, chromeArgs, {
+                detached: true,
+                stdio: ['ignore']
+            });
+            this._chromeProc.unref();
+            this._chromeProc.on('error', (err) => {
+                logger.log('chrome error: ' + err);
+                this.terminateSession();
+            });
 
-        logger.log(`spawn('${chromePath}', ${JSON.stringify(chromeArgs) })`);
-        this._chromeProc = spawn(chromePath, chromeArgs, {
-            detached: true,
-            stdio: ['ignore']
+            return this.doAttach(port, launchUrl, args.address);
         });
-        this._chromeProc.unref();
-        this._chromeProc.on('error', (err) => {
-            logger.log('chrome error: ' + err);
-            this.terminateSession();
-        });
-
-        return this.doAttach(port, launchUrl, args.address);
     }
 
     public disconnect(): Promise<void> {
