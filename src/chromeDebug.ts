df481610cbe460c99fd8bc68533d8b81f7ab507c
Rob Lourens <roblourens@gmail.com>
2016-09-04 15:53:37 -0700

    Pass extensionName for errors

--- src/chromeDebug.ts
+++ src/chromeDebug.ts
@@ -7,13 +7,16 @@ import * as path from 'path';
 
 import {ChromeDebugAdapter} from './chromeDebugAdapter';
 
+const EXTENSION_NAME = 'debugger-for-chrome';
+
 // Start a ChromeDebugSession configured to only match 'page' targets, which are Chrome tabs.
 // Cast because DebugSession is declared twice - in this repo's vscode-debugadapter, and that of -core... TODO
 ChromeDebugSession.run(<any>ChromeDebugSession.getSession(
     {
         logFilePath: path.resolve(__dirname, '../../vscode-chrome-debug.txt'), // non-.txt file types can't be uploaded to github
-        adapter: new ChromeDebugAdapter(new ChromeConnection(undefined, target => target && (!target.type || target.type === 'page')))
+        adapter: new ChromeDebugAdapter(new ChromeConnection(undefined, target => target && (!target.type || target.type === 'page'))),
+        extensionName: EXTENSION_NAME
     }));
 
 /* tslint:disable:no-var-requires */
-logger.log('debugger-for-chrome: ' + require('../../package.json').version);
+logger.log(EXTENSION_NAME + ': ' + require('../../package.json').version);
