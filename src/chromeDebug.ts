ebe671a9699ae7b3122c117c93da8c7598fbca7c
Rob Lourens <roblourens@gmail.com>
2016-09-19 18:09:39 +0200

    Add service overrides for latest -core

--- src/chromeDebug.ts
+++ src/chromeDebug.ts
@@ -2,21 +2,27 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
-import {ChromeDebugSession, logger, ChromeConnection} from 'vscode-chrome-debug-core';
+import {ChromeDebugSession, logger, ChromeConnection, UrlPathTransformer, BaseSourceMapTransformer} from 'vscode-chrome-debug-core';
 import * as path from 'path';
 
 import {ChromeDebugAdapter} from './chromeDebugAdapter';
 
 const EXTENSION_NAME = 'debugger-for-chrome';
+const targetFilter = target => target && (!target.type || target.type === 'page');
 
 // Start a ChromeDebugSession configured to only match 'page' targets, which are Chrome tabs.
 // Cast because DebugSession is declared twice - in this repo's vscode-debugadapter, and that of -core... TODO
-ChromeDebugSession.run(<any>ChromeDebugSession.getSession(
+ChromeDebugSession.run(ChromeDebugSession.getSession(
     {
         logFilePath: path.resolve(__dirname, '../../vscode-chrome-debug.txt'), // non-.txt file types can't be uploaded to github
-        adapter: new ChromeDebugAdapter(new ChromeConnection(undefined, target => target && (!target.type || target.type === 'page'))),
-        extensionName: EXTENSION_NAME
+        adapter: ChromeDebugAdapter,
+        extensionName: EXTENSION_NAME,
+        targetFilter,
+
+        pathTransformer: UrlPathTransformer,
+        sourceMapTransformer: BaseSourceMapTransformer
     }));
 
 /* tslint:disable:no-var-requires */
 logger.log(EXTENSION_NAME + ': ' + require('../../package.json').version);
+/* tslint:enable:no-var-requires */
