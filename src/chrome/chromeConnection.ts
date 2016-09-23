793a283123a509d3b668f048e0d79e818a96d6c2
Rob Lourens <roblourens@gmail.com>
2016-09-23 17:00:33 +0200

    Pass along returnByValue

--- src/chrome/chromeConnection.ts
+++ src/chrome/chromeConnection.ts
@@ -257,7 +257,7 @@ export class ChromeConnection {
     }
 
     public runtime_evaluate(expression: string, objectGroup?: string, contextId = 1, returnByValue = false, silent?: boolean): Promise<Chrome.Runtime.EvaluateResponse> {
-        return this.sendMessage('Runtime.evaluate', <Chrome.Runtime.EvaluateParams>{ expression, objectGroup, contextId, silent });
+        return this.sendMessage('Runtime.evaluate', <Chrome.Runtime.EvaluateParams>{ expression, objectGroup, contextId, returnByValue, silent });
     }
 
     public runtime_callFunctionOn(objectId: string, functionDeclaration: string, args?: Chrome.Runtime.CallArgument[], silent?: boolean, returnByValue?: boolean,
