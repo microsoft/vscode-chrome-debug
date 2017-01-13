defe3ac197934ec73b8c5e875c647f4755b6674e
roblou <roblourens@gmail.com>
2017-01-13 15:17:50 -0800

    Replace socket with actual custom debug command

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,6 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-import * as net from 'net';
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('extension.chrome-debug.addFrameToSkipFiles', addFrameToSkipFiles));
@@ -14,22 +13,11 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {
 }
 
-function addFrameToSkipFiles(url: any): void {
-    console.log(`addFrameToSkipFiles`);
-    console.log(url);
+function addFrameToSkipFiles(path: string): void {
+    if (!path) {
+        const activeEditor = vscode.window.activeTextEditor;
+        path = activeEditor && activeEditor.document.fileName;
+    }
 
-    const socket = net.connect({ port: 7891 }, () => {
-        socket.write(url);
-    });
-
-    socket.on("data", (data: any) => {
-    });
-
-    socket.on("error", function(reason: Error) {
-        console.log(`socket error: ` + reason);
-    });
-
-    socket.on("end", function() {
-        console.log(`socket end`);
-    });
+    vscode.commands.executeCommand('workbench.customDebugRequest', 'toggleSkipFileStatus', { path });
 }
