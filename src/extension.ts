1586ec657849795541df8ced39feedd6218ecae6
roblou <roblourens@gmail.com>
2017-01-19 11:09:24 -0800

    tslint

--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,6 @@
  *--------------------------------------------------------*/
 
 import * as vscode from 'vscode';
-import * as path from 'path';
-import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('extension.chrome-debug.toggleSkippingFile', toggleSkippingFile));
