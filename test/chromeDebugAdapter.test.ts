773df8d04c66901cc374506466e354d547051353
Amit Mittal <eramitmittal@users.noreply.github.com>
2017-11-10 18:51:12 +0100

    Update chromeDebugAdapter.test.ts

--- test/chromeDebugAdapter.test.ts
+++ test/chromeDebugAdapter.test.ts
@@ -138,7 +138,7 @@ suite('ChromeDebugAdapter', () => {
                 { '/src': WEBROOT + '/app/src' });
             assert.deepEqual(
                 resolveWebRootPattern(WEBROOT, <ISourceMapPathOverrides>{ '${webRoot}/src': '${webRoot}/app/src'}),
-                { WEBROOT + '/src': WEBROOT + '/app/src' });
+                { (WEBROOT + '/src'): (WEBROOT + '/app/src') });
         });
 
         test(`ignores the webRoot pattern when it's not at the beginning of the string`, () => {
