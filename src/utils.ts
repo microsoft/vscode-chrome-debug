800b3b235373bdd9337c75fa8d4951a08eef51ea
xymopen <xuyiming.open@outlook.com>
2018-01-26 21:37:23 +0800

    add default installion location of release channels other than stable

--- src/utils.ts
+++ src/utils.ts
@@ -6,12 +6,28 @@ import * as path from 'path';
 import {utils as coreUtils, chromeConnection} from 'vscode-chrome-debug-core';
 
 const WIN_APPDATA = process.env.LOCALAPPDATA || '/';
+const WIN_PROGFILES = process.env.ProgramFiles || '/';
+const WIN_PROGFILESx86 = process.env['ProgramFiles(x86)'] || '/';
+
+const WIN_DEV_LOC = 'Google\\Chrome Dev\\Application\\chrome.exe';
+const WIN_BETA_LOC = 'Google\\Chrome Beta\\Application\\chrome.exe';
+const WIN_STABLE_LOC = 'Google\\Chrome\\Application\\chrome.exe';
+
 const DEFAULT_CHROME_PATH = {
     LINUX: '/usr/bin/google-chrome',
     OSX: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-    WIN: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-    WIN_LOCALAPPDATA: path.join(WIN_APPDATA, 'Google\\Chrome\\Application\\chrome.exe'),
-    WINx86: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+
+    WIN_LOCALAPPDATA_DEV: path.join(WIN_APPDATA, WIN_DEV_LOC),
+    WIN_LOCALAPPDATA_BETA: path.join(WIN_APPDATA, WIN_BETA_LOC),
+    WIN_LOCALAPPDATA_STABLE: path.join(WIN_APPDATA, WIN_STABLE_LOC),
+
+    WIN_DEV: path.join(WIN_PROGFILES, WIN_DEV_LOC),
+    WIN_BETA: path.join(WIN_PROGFILES, WIN_BETA_LOC),
+    WIN_STABLE: path.join(WIN_PROGFILES, WIN_STABLE_LOC),
+
+    WINx86_DEV: path.join(WIN_PROGFILESx86, WIN_DEV_LOC),
+    WINx86_BETA: path.join(WIN_PROGFILESx86, WIN_BETA_LOC),
+    WINx86_STABLE: path.join(WIN_PROGFILESx86, WIN_STABLE_LOC),
 };
 
 export function getBrowserPath(): string {
@@ -19,12 +35,24 @@ export function getBrowserPath(): string {
     if (platform === coreUtils.Platform.OSX) {
         return coreUtils.existsSync(DEFAULT_CHROME_PATH.OSX) ? DEFAULT_CHROME_PATH.OSX : null;
     } else if (platform === coreUtils.Platform.Windows) {
-        if (coreUtils.existsSync(DEFAULT_CHROME_PATH.WINx86)) {
-            return DEFAULT_CHROME_PATH.WINx86;
-        } else if (coreUtils.existsSync(DEFAULT_CHROME_PATH.WIN)) {
-            return DEFAULT_CHROME_PATH.WIN;
-        } else if (coreUtils.existsSync(DEFAULT_CHROME_PATH.WIN_LOCALAPPDATA)) {
-            return DEFAULT_CHROME_PATH.WIN_LOCALAPPDATA;
+        if (coreUtils.existsSync(DEFAULT_CHROME_PATH.WIN_LOCALAPPDATA_DEV)) {
+            return DEFAULT_CHROME_PATH.WIN_LOCALAPPDATA_DEV;
+        } else if (coreUtils.existsSync(DEFAULT_CHROME_PATH.WIN_LOCALAPPDATA_BETA)) {
+            return DEFAULT_CHROME_PATH.WIN_LOCALAPPDATA_BETA;
+        } else if (coreUtils.existsSync(DEFAULT_CHROME_PATH.WIN_LOCALAPPDATA_STABLE)) {
+            return DEFAULT_CHROME_PATH.WIN_LOCALAPPDATA_STABLE;
+        } else if (coreUtils.existsSync(DEFAULT_CHROME_PATH.WIN_DEV)) {
+            return DEFAULT_CHROME_PATH.WIN_DEV;
+        } else if (coreUtils.existsSync(DEFAULT_CHROME_PATH.WIN_BETA)) {
+            return DEFAULT_CHROME_PATH.WIN_BETA;
+        } else if (coreUtils.existsSync(DEFAULT_CHROME_PATH.WIN_STABLE)) {
+            return DEFAULT_CHROME_PATH.WIN_STABLE;
+        } else if (coreUtils.existsSync(DEFAULT_CHROME_PATH.WINx86_DEV)) {
+            return DEFAULT_CHROME_PATH.WINx86_DEV;
+        } else if (coreUtils.existsSync(DEFAULT_CHROME_PATH.WINx86_BETA)) {
+            return DEFAULT_CHROME_PATH.WINx86_BETA;
+        } else if (coreUtils.existsSync(DEFAULT_CHROME_PATH.WINx86_STABLE)) {
+            return DEFAULT_CHROME_PATH.WINx86_STABLE;
         } else {
             return null;
         }
