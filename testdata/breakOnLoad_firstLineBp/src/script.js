aba7742e08df730cdc11468273de6b7806dbea4f
Diego Geffner <digeff@microsoft.com>
2018-02-15 09:47:10 -0800

    Added new integration test

---
+++ testdata/breakOnLoad_firstLineBp/src/script.js
@@ -0,0 +1,7 @@
+document.write("Script.js file"); console.log("Hi");
+var a = 1;
+var b = 1; fun();
+
+function fun() {
+    return true;
+}
---
+++ testdata/breakOnLoad_firstLineBp/src/script2.js
@@ -0,0 +1,7 @@
+document.write("Script2.js file"); console.log("Hi2");
+var a = 1;
+var b = 1; fun2();
+
+function fun2() {
+    return true;
+}
