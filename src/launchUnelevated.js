03b0ad8b2163ff025bbe9d59cdd8277e17d896a3
digeff <digeff@hotmail.com>
2018-05-14 15:28:14 -0700

    Applied feedback

--- src/launchUnelevated.js
+++ src/launchUnelevated.js
@@ -28,7 +28,8 @@ if (objShellWindows != null)
 
     if (objShellWindows.count === 0) {
         // If no File Explorer windows are open fall-back to this alternative way of executing the command
-        var item = objShellWindows.FindWindowSW(/*NULL*/0, /*NULL*/0, /*SWC_DESKTOP*/8, /*NULL*/0, /*SWFO_NEEDDISPATCH*/1);
+        // See IShellWindows::FindWindowSW at https://msdn.microsoft.com/en-us/library/windows/desktop/cc836568(v=vs.85).aspx
+        var item = objShellWindows.FindWindowSW(/*pvarLoc=NULL*/0, /*pvarLocRoot=NULL*/0, /*SWC_DESKTOP*/8, /*phwnd=NULL*/0, /*SWFO_NEEDDISPATCH*/1);
         item.Document.Application.ShellExecute(command, params);
     }
 }
