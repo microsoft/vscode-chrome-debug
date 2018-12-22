4fccf75935c0926e577204ba9c34d961a00e4510
Rob Lourens <roblourens@gmail.com>
2018-12-22 16:49:36 -0600

    Tweaks so it can start

--- src/chromeDebug.ts
+++ src/chromeDebug.ts
@@ -2,13 +2,12 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
-import { ChromeDebugSession, logger, UrlPathTransformer, BaseSourceMapTransformer, telemetry } from 'vscode-chrome-debug-core';
-import * as path from 'path';
 import * as os from 'os';
+import * as path from 'path';
+import { BaseSourceMapTransformer, ChromeDebugSession, logger, OnlyProvideCustomLauncherExtensibilityPoints, telemetry, UrlPathTransformer } from 'vscode-chrome-debug-core';
+import { ChromeDebugAdapter } from './chromeDebugAdapter';
+import { ChromeLauncher } from './chromeLauncher';
 import { defaultTargetFilter } from './utils';
-
-import { ChromeDebugAdapter } from 'vscode-chrome-debug-core';
-import { ExtensibilityPoints } from 'vscode-chrome-debug-core/lib/src/chrome/extensibility/extensibilityPoints';
 
 const EXTENSION_NAME = 'debugger-for-chrome';
 
@@ -20,13 +19,13 @@ ChromeDebugSession.run(ChromeDebugSession.getSession(
         extensionName: EXTENSION_NAME,
         logFilePath: path.resolve(os.tmpdir(), 'vscode-chrome-debug.txt'),
         targetFilter: defaultTargetFilter,
-        extensibilityPoints: new ExtensibilityPoints(),
+        extensibilityPoints: new OnlyProvideCustomLauncherExtensibilityPoints(ChromeLauncher),
         pathTransformer: UrlPathTransformer,
         sourceMapTransformer: BaseSourceMapTransformer,
     }));
 
 /* tslint:disable:no-var-requires */
-const debugAdapterVersion = require('../../package.json').version;
+const debugAdapterVersion = require('../package.json').version;
 logger.log(EXTENSION_NAME + ': ' + debugAdapterVersion);
 
 /* __GDPR__FRAGMENT__
