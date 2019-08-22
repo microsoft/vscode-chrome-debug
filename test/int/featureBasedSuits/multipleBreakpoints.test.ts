30b057dc79689900f0c09e7211463b85362467ac
digeff <digeff@users.noreply.github.com>
2019-08-22 11:27:52 -0700

    Added copyright

--- test/int/featureBasedSuits/multipleBreakpoints.test.ts
+++ test/int/featureBasedSuits/multipleBreakpoints.test.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { puppeteerSuite, puppeteerTest } from '../puppeteer/puppeteerSuite';
 
 import { reactTestSpecification } from '../resources/resourceProjects';
